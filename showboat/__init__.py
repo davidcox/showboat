ddf67fe3041dae9cbe741d4e712b2da25bd901e6
David Cox <david.daniel.cox@gmail.com>
2012-02-01 10:57:16 -0500

    (Python)Tidy-ing up

--- showboat/__init__.py
+++ showboat/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from util import *
 from pkg_resources import resource_filename
 import shutil
@@ -6,70 +8,70 @@ import shutil
 def deposit_skeleton(dst_path):
     if not os.path.exists(dst_path):
         os.mkdir(dst_path)
-    
+
     jade_skel_path = resource_filename(__name__, 'payload/slides.jade')
-    
+
     shutil.copy(jade_skel_path, dst_path)
-    
+
     os.mkdir(os.path.join(dst_path, 'scripts'))
     os.mkdir(os.path.join(dst_path, 'styles'))
 
+
 def compile(src_path, out_path, copy_assets=False, overwrite_output=True,
             build_thumbnails=True):
-        
+
     if overwrite_output:
         # remove the existing output dir
         try:
             shutil.rmtree(out_path)
         except:
             pass
-    
-    print("Outputting presentation to %s..." % out_path)
-    
+
+    print 'Outputting presentation to %s...' % out_path
+
     os.mkdir(out_path)
-    
-    
+
     global_styles_path = resource_filename(__name__, 'payload/styles')
     global_scripts_path = resource_filename(__name__, 'payload/scripts')
     local_styles_path = os.path.join(src_path, 'styles')
     local_scripts_path = os.path.join(src_path, 'scripts')
-    
+
     # rsync in the supporting file directories
     rsync(global_styles_path, out_path)
     rsync(global_scripts_path, out_path)
     rsync(local_styles_path, out_path)
     rsync(local_scripts_path, out_path)
-    
+
     # compile the coffeescript
     coffeescript_compile(out_path)
-    
+
     local_assets_path = os.path.join(src_path, 'assets')
     local_assets_path = os.path.abspath(os.path.expanduser(local_assets_path))
-    
+
     if os.path.exists(local_assets_path):
         assets_path = local_assets_path
     else:
         assets_path = os.path.abspath(os.path.expanduser(config['assets_path']))
-    
+
     # symlink or copy in the global assets, if they exist
     if os.path.exists(assets_path):
         for item in os.listdir(assets_path):
             if item == '.DS_Store':
                 continue
-        
+
             asset_src = os.path.join(assets_path, item)
             asset_dst = os.path.join(out_path, item)
-        
+
             if copy_assets:
                 shutil.copytree(asset_src, asset_dst)
             else:
                 os.symlink(asset_src, asset_dst)
     else:
-        print("No assets directory found")
-        
+        print 'No assets directory found'
+
     # compile the jade
     jade_compile(src_path, out_path)
-    
+
     if build_thumbnails:
         # build thumbnail pngs for all of the slides
         # count the number of slides
--- showboat/util.py
+++ showboat/util.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import re
-from pkg_resources import resource_filename, resource_string
-import simplejson as json
+from pkg_resources import resource_filename
 import shlex
-import shutil
 import subprocess
 from mako.template import Template
 import urllib
@@ -11,23 +11,26 @@ import time
 from contextlib import contextmanager
 from ConfigParser import SafeConfigParser
 
-
 showboat_keywords = ('slide', 'build', 'notes', 'svg_include')
 top_level_module_name = __name__.split('.')[0]
 
+
 # Configuration details
+
 def read_config(cfg_path):
     cp = SafeConfigParser()
     cp.read(cfg_path)
-    
+
     d = {}
     for section in cp.sections():
         d.update(dict(cp.items(section)))
     return d
 
+
 def default_config():
-    return read_config(resource_filename(top_level_module_name, 
-                                  'config/default.config'))
+    return read_config(resource_filename(top_level_module_name,
+                       'config/default.config'))
+
 
 def load_config():
     # check for .plotsk in ~/
@@ -37,9 +40,12 @@ def load_config():
     # shortcut
     return default_config()
 
+
 config = load_config()
 
+
 # work around bizarro pkg_resources / MACOSX_DEPLOYMENT_TARGET insanity
+
 def syscall(cmd):
     os.unsetenv('MACOSX_DEPLOYMENT_TARGET')
     return subprocess.call(shlex.split(str(cmd)))
@@ -49,23 +55,25 @@ def rsync(src, dst):
     cmd = Template(config['sync_cmd']).render(src_path=src, dst_path=dst)
     syscall(cmd)
 
+
 def preprocess_jade(jade_str):
-    
+
     for kw in showboat_keywords:
         jade_str = re.sub(r'%s(\s|\(|\.)' % kw, r'.%s\1' % kw, jade_str)
-    
+
     return jade_str
 
+
 def jade_compile(src_path, dst_path):
-    
+
     resource_path = resource_filename(top_level_module_name, 'payload/')
-    print("resource path: %s" % os.listdir(resource_path))
-    
+    print 'resource path: %s' % os.listdir(resource_path)
+
     src_files = [os.path.join(src_path, f) for f in os.listdir(src_path)]
-    resource_files = [os.path.join(resource_path, f) for f in 
+    resource_files = [os.path.join(resource_path, f) for f in
                       os.listdir(resource_path)]
 
-    for fp in (resource_files + src_files):
+    for fp in resource_files + src_files:
         try:
             if fp.split('.')[-1] == 'jade':
                 full_path = os.path.abspath(os.path.expanduser(fp))
@@ -75,8 +83,8 @@ def jade_compile(src_path, dst_path):
                     preproc = preprocess_jade(f_str)
                     with open(os.path.join(dst_path, fn), 'w') as out_f:
                         out_f.write(preproc)
+        except Exception, e:
                 # shutil.copy(, dst_path)
-        except Exception as e:
             print e
             pass
     jade_cmd = Template(config['html_compile_cmd']).render(dst_path=dst_path)
@@ -87,77 +95,75 @@ def coffeescript_compile(src_path):
     script_path = os.path.join(src_path, 'scripts')
     cmd = Template(config['script_compile_cmd']).render(script_path=script_path)
     syscall(cmd)
-    
+
+
 def count_n_slides(out_path):
     with open(os.path.join(out_path, 'index.html'), 'r') as f:
         html = '\n'.join(f.readlines())
         n = len(re.findall(r'class\s*\=\s*\"slide\"', html))
         return n
 
+
 @contextmanager
 def serve_http(root_path='.', host='localhost', port=8080, timeout=5.0,
                src_path='<undefined>'):
-               
+
     cmd = Template(config['http_server_cmd']).render(root_path=root_path,
-                                                     src_path=src_path,
-                                                     host=host,
-                                                     port=port)
+            src_path=src_path, host=host, port=port)
     proc = subprocess.Popen(shlex.split(str(cmd)))
-    
+
     # check to see if the server is up yet
     connected = False
     tic = time.time()
 
-
-    while not connected and ((time.time() - tic) < timeout):
+    while not connected and time.time() - tic < timeout:
         try:
-            urllib.urlopen("http://localhost:8080/index.html")
+            urllib.urlopen('http://localhost:8080/index.html')
             connected = True
         except IOError:
             pass
 
     if not connected:
         raise IOError('Unable to connect to internally-launched HTTP server')
-        
+
     yield proc
 
     proc.kill()
 
+
 def build_slide_thumbnails(dst_path, n_slides, async=True):
 
-    print('Building thumbnails... ')
+    print 'Building thumbnails... '
     tic = time.time()
-    
+
     with serve_http(dst_path):
-    
+
         tn_path = os.path.join(dst_path, 'thumbnails')
         os.mkdir(tn_path)
-    
+
         def generate_one_thumbnail(n):
-            slide_url = "http://localhost:8080/index.html#%d" % (n+1)
+            slide_url = 'http://localhost:8080/index.html#%d' % (n + 1)
             cmd = Template(config['thumbnail_cmd']).render(width=200,
-                                                           height=150,
-                                                           dst_path=tn_path,
-                                                           slide_number = n+1,
-                                                           slide_url = slide_url)
+                    height=150, dst_path=tn_path, slide_number=n + 1,
+                    slide_url=slide_url)
             syscall(cmd)
 
         if async:
             from threading import Thread
             from functools import partial
-            
+
             batch_size = 4
-            
-            for b in range(0, (n_slides / batch_size) + 1):
-                low = b  * batch_size
-                high = (b+1) * batch_size - 1
-                
+
+            for b in range(0, n_slides / batch_size + 1):
+                low = b * batch_size
+                high = (b + 1) * batch_size - 1
+
                 if low > n_slides:
                     break
                 if high > n_slides:
                     high = n_slides
-                    
-                threads = [Thread(target=partial(generate_one_thumbnail, n)) \
+
+                threads = [Thread(target=partial(generate_one_thumbnail, n))
                            for n in range(low, high)]
                 for t in threads:
                     t.start()
@@ -166,39 +172,42 @@ def build_slide_thumbnails(dst_path, n_slides, async=True):
         else:
             map(generate_one_thumbnail, range(0, n_slides))
 
-    print('Done building thumbnails (took %f seconds).' % (time.time()-tic) )
+    print 'Done building thumbnails (took %f seconds).' % (time.time() - tic)
+
 
 def view_url(url, use_alt=False):
-    
+
     if use_alt:
         app_key = 'alt_view_url_cmd'
     else:
         app_key = 'view_url_cmd'
-    
+
     # check if it is a file
     if os.path.exists(url):
-        url = "file://" + url
+        url = 'file://' + url
     cmd = Template(config[app_key]).render(url=url)
     print cmd
     syscall(cmd)
 
+
 def present_url(url):
     # check if it is a file
     if os.path.exists(url):
-        url = "file://" + url
+        url = 'file://' + url
     cmd = Template(config['present_url_cmd']).render(url=url)
     syscall(cmd)
+
 
 def launch_external_svg_editor(path):
     file_path = os.path.join(config['assets_path'], path)
     file_path = os.path.abspath(os.path.expanduser(file_path))
-    
+
     split_path = os.path.split(file_path)
     file_name = split_path[-1]
-    file_name_noext = ".".join(file_name.split(".")[0:-1])
-    file_ext = file_name.split(".")[-1]
+    file_name_noext = '.'.join(file_name.split('.')[0:-1])
+    file_ext = file_name.split('.')[-1]
     containing_path = os.path.join(*split_path[0:-1])
-    
+
     cmd_template = config['edit_svg_cmd']
     cmd = Template(cmd_template).render(file_path=file_path,
                                         file_name=file_name,
@@ -208,11 +217,12 @@ def launch_external_svg_editor(path):
     syscall(cmd)
     return cmd
 
+
 def save_svg_file(path, contents, viewbox_hack=True):
-    
+
     file_path = os.path.join(config['assets_path'], path)
     file_path = os.path.abspath(os.path.expanduser(file_path))
-    
+
     if viewbox_hack:
         import re
         # a quick hack to restore the viewbox, which svg-edit destroys
@@ -221,10 +231,9 @@ def save_svg_file(path, contents, viewbox_hack=True):
             vbs = re.findall(r'viewBox\s*=\s*\"(.+?)\"', old_content)
             if len(vbs) == 1:
                 vb = vbs[0]
-                
+
                 # hacky hack hack hack
-                contents = re.sub('<svg', '<svg viewBox="%s"'%vb, contents)
-    
+                contents = re.sub('<svg', '<svg viewBox="%s"' % vb, contents)
+
     with open(file_path, 'w') as f:
         f.write(contents)
-    
